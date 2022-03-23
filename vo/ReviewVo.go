28458ec847364653fd87287bdb89887573192c94
wzm <wangziming2022@163.com>
2022-03-23 17:52:53 +0800

    修复审核列表时间显示问题

--- vo/ReviewVo.go
+++ vo/ReviewVo.go
@@ -10,20 +10,9 @@ type ReviewVideoVo struct {
 	Partition string `json:"partition"` //分区名，3.6.8新增
 }
 
-//管理员视频列表
-type AdminVideoVo struct {
-	ID        uint      `json:"vid"`
-	Title     string    `json:"title"`
-	Cover     string    `json:"cover"`
-	Desc      string    `json:"desc"`
-	CreateAt  time.Time `json:"create_at"`
-	Copyright bool      `json:"copyright"`
-	Uid       uint      `json:"uid"`
-}
-
 type ReviewVideoListVo struct {
 	ID        uint      `json:"vid"`
-	CreateAt  time.Time `json:"create_at"`
+	CreatedAt time.Time `json:"created_at"`
 	Title     string    `json:"title"`
 	Cover     string    `json:"cover"`
 	VideoType string    `json:"video_type"`
